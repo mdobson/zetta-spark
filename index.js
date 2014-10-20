3d0acc9a61f6dda76973d3ceec6b54bd4b662522
Matt Dobson <mdobson4@gmail.com>
2014-10-20 08:16:09 -0400

    Refactored snippet into server that will track devices that come online and allow access in node space for them eventemitter style.

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
+
+
 var DeviceServer = require('spark-protocol').DeviceServer;
 var EventEmitter = require('events').EventEmitter;
-var server = new DeviceServer({
-  coreKeysDir: '.'
-});
+var util = require('util');
 
-var emitter = new EventEmitter();
+var WrappedServer = module.exports = function(opts) {
+  EventEmitter.call(this);
+  opts = opts || {};
+  var keys = opts.coreKeysDir || '.';
+  this.server = new DeviceServer({
+    coreKeysDir:keys
+  });
+  
+  //No idea why they set a global. 
+  global.server = this.server;
+  this.discoveredCores = [];
+};
+util.inherits(WrappedServer, EventEmitter);
 
-global.server = server;
-var interval = setInterval(function(){
-  var cores = global.server.getAllCores();
-  var myCoreIds = Object.keys(cores);
-  if(myCoreIds.length) {
-    var myCore = cores[myCoreIds[0]];
-    emitter.emit('device', myCore);
+WrappedServer.prototype.search = function() {
+  var self = this;
+  this._interval = setInterval(function(){
+    var cores = global.server.getAllCores();
+    var myCoreIds = Object.keys(cores);
+    if(myCoreIds.length) {
+      myCoreIds.forEach(function(coreId) {
+        if(self.discoveredCores.indexOf(coreId) === -1) {
+          var myCore = cores[coreId];
+          self.emit('device', myCore);
+          self.discoveredCores.push(coreId);
+        }
+      });
+    }
+  }, 3000);
+};
+
+WrappedServer.prototype.stopSearch = function() {
+  if(this._interval) {
+    clearInterval(this._interval);
   }
-}, 3000);
+};
 
-emitter.on('device', function(device){
+WrappedServer.prototype.start = function() {
+  server.start();
+  this.search();
+}
+
+var deviceServer = new WrappedServer();
+
+deviceServer.on('device', function(device){
   console.log('device online in user space');
-  clearInterval(interval);
   device.onApiMessage('matt', { cmd:'Ping'});
   device.on('matt', function(sender, args) {
     console.log('Matt event');
   });
 });
-server.start();
+
+deviceServer.start();
 
