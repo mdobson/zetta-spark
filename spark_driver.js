2ac4e457d83150c5cc24056a723387472a723e4e
Matt Dobson <mdobson4@gmail.com>
2014-10-22 21:48:10 -0400

    Updates to reflect tinker functionality within the firmware.

--- spark_driver.js
+++ spark_driver.js
@@ -15,9 +15,11 @@ Spark.prototype.init = function(config) {
     .type('spark')
     .name(this._spark.coreID)
     .state('online')
-    .when('online', { allow: ['ping', 'digitalWrite', 'digitalRead'] })
+    .when('online', { allow: ['ping', 'digitalWrite', 'digitalRead', 'analogRead', 'analogWrite'] })
     .map('digitalWrite', this.digitalWrite, [{name: 'pin', type: 'text'}, {name:'state', type:'text'}])
     .map('digitalRead', this.digitalRead, [{name: 'pin', type: 'text'}])
+    .map('analogRead', this.analogRead, [{name: 'pin', type: 'text'}])
+    .map('analogWrite', this.analogWrite, [{name: 'pin', type: 'text'}, {name: 'value', type:'text'}])
     .map('ping', this.ping);
 };
 
@@ -73,14 +75,42 @@ Spark.prototype.digitalWrite = function(pin, state, cb) {
   });
 };
 
-Spark.prototype.digitalRead = function(cb) {
+Spark.prototype.digitalRead = function(pin, cb) {
   var self = this;
-  var pin = 'D7';
   var args = [pin];
   this.state = 'reading-pin';
   this._sendMessage('CallFn', 'digitalread', args, function(err, event, result) {
     self.state = 'online';
     self[pin] = result.result;
+    if(cb) {
+      cb();
+    }
+  });
+};
+
+Spark.prototype.analogRead = function(pin, cb) {
+  var self = this;
+  var args = [pin];
+  this.state = 'reading-pin';
+  this._sendMessage('CallFn', 'analogread', args, function(err, event, result) {
+    self.state = 'online';
+    self[pin] = result.result;
+    if(cb) {
+      cb();
+    }
+  });
+};
+
+Spark.prototype.analogWrite= function(pin, value, cb) {
+  var self = this;
+  var args = [[pin, state].join(',')];
+  this.state = 'setting-pin';
+  this._sendMessage('CallFn', 'analogwrite', args, function(err, event, result) {
+    console.log(err);
+    self.state = 'online';
+    if(result.result) {
+      self[pin] = state;
+    }
     if(cb) {
       cb();
     }
